bb49c444058e1deb0e15c05c0191689a28909ba8
Kamil Wais <kamil.wais@gmail.com>
2024-12-06 08:25:21 +0100

    added docs building

--- devel/deploy.R
+++ devel/deploy.R
@@ -6,3 +6,10 @@ renv::upgrade()
 renv::update()
 
 usethis::use_version(which = "dev")
+# usethis::use_version(which = "patch")
+# usethis::use_version(which = "minor")
+
+devtools::build_readme()
+pkgdown::build_site(preview = FALSE)
+pkgdown::preview_site()
+
