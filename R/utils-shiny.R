42372af5f8c81b9d9208200aa5e6054a80ba0ac2
Kamil Wais <kamil.wais@gmail.com>
2024-12-14 18:51:08 +0100

    added package version to footer

--- R/utils-shiny.R
+++ R/utils-shiny.R
@@ -1,31 +1,39 @@
 sidebar_footer <- function(hex_size = "110px") {
 
-  shiny::div(
-    style = 
-      "text-align: center; display: flex; justify-content: center; gap: 10px;",
-    shiny::tags$a(
-      shiny::tags$img(
-        src = "figures/r4ga-logo.png",
-        height = hex_size,
-        width = hex_size,
-        alt = "R4Good.Academy",
-        style = "display: block; margin: 0; border: none;"
+  shiny::tagList(
+    shiny::div(
+      style = 
+        "text-align: center; display: flex; justify-content: center; gap: 10px;",
+      shiny::tags$a(
+        shiny::tags$img(
+          src = "figures/r4ga-logo.png",
+          height = hex_size,
+          width = hex_size,
+          alt = "R4Good.Academy",
+          style = "display: block; margin: 0; border: none;"
+        ),
+        href = "https://r4good.academy/",
+        target = "_blank"
       ),
-      href = "https://r4good.academy/",
-      target = "_blank"
+      shiny::tags$a(
+        shiny::tags$img(
+          src = "figures/logo.png",
+          height = hex_size,
+          width = hex_size,
+          alt = "R4GoodPersonalFinances",
+          style = "display: block; margin: 0; border: none;"
+        ),
+        href = "https://r4goodacademy.github.io/R4GoodPersonalFinances",
+        target = "_blank"
+      )
     ),
-    shiny::tags$a(
-      shiny::tags$img(
-        src = "figures/logo.png",
-        height = hex_size,
-        width = hex_size,
-        alt = "R4GoodPersonalFinances",
-        style = "display: block; margin: 0; border: none;"
-      ),
-      href = "https://r4goodacademy.github.io/R4GoodPersonalFinances",
-      target = "_blank"
+    shiny::p(
+      as.character(packageVersion("R4GoodPersonalFinances")), 
+      style = 
+        "text-align: center; font-size: 9px; margin-top: -26px; padding: 0;"
     )
   )
+
 }
 
 add_sidebar_footer_resources <- function() {
