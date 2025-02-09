d1648ac8b2f838ef7310555da7902ffe5ce13413
Kamil Wais <kamil.wais@gmail.com>
2025-02-09 13:43:24 +0100

    refactored code

--- R/calc_retirement_ruin.R
+++ R/calc_retirement_ruin.R
@@ -1,27 +1,53 @@
+# See: Milevsky, M.A. (2020). Retirement Income Recipes in R: From Ruin Probabilities to Intelligent Drawdowns. Use R! Series. https://doi.org/10.1007/978-3-030-51434-1
 #' @export
 calc_retirement_ruin <- function(
-  yearly_spendings,
-  portfolio_value,
   portfolio_return_mean,
   portfolio_return_sd,
-  expected_lifetime
+  age,
+  gompertz_mode,
+  gompertz_dispersion,
+  portfolio_value,
+  monthly_spendings,
+  yearly_spendings = 12 * monthly_spendings,
+  spending_rate    = yearly_spendings / portfolio_value
 ) {
-  
-  spending_rate <- yearly_spendings / portfolio_value
-  
-  lambda <- 1 / expected_lifetime # implied mortality rate
 
-  alpha <- 
-    (2 * portfolio_return_mean + 4 * lambda) / 
-    (portfolio_return_sd^2 + lambda) - 1
+    nu    <- portfolio_return_mean
+    sigma <- portfolio_return_sd
+    x     <- age
+    m     <- gompertz_mode
+    b     <- gompertz_dispersion
+  
+    mu <- nu + (0.5) * sigma^2 
 
-  beta <-
-    (portfolio_return_sd^2 + lambda) / 2
+    M1 <- calc_a(mu - sigma^2, x, m, b)
+  
+    M2 <- 
+      # (calc_a(mu - sigma^2, x, m, b)
+      (M1 - calc_a(2 * mu - 3 * sigma^2, x, m, b)) / (mu / 2 - sigma^2)
+    
+    alpha <- (2 * M2 - M1^2) / (M2 - M1^2)
+    beta  <- (M2 - M1^2) / (M2 * M1)
 
   pgamma(
-    q = spending_rate,
-    shape = alpha,
-    scale = beta
+    q          = spending_rate,
+    shape      = alpha,
+    scale      = beta,
+    lower.tail = TRUE
   )
+}
 
+calc_a <- function(v, x, m, b) {
+  
+  b * exp(exp((x - m) / b) + (x - m) * v) * 
+    calc_incomplete_gamma(-b * v, exp((x - m) / b))
+}
+
+calc_incomplete_gamma <- function(a, c) {
+
+  integrand <- function(t) {
+    t^(a - 1) * exp(-t)
+  }
+
+  integrate(integrand, c, Inf)$value
 }
--- tests/testthat/test-calc_retirement_ruin.R
+++ tests/testthat/test-calc_retirement_ruin.R
@@ -1,32 +1,69 @@
-test_that("calculating retirement ruin probability", {
+test_that("calculating gompertz retirement ruin probability", {
 
-  ruin_probability <- 
-    calc_retirement_ruin(
-      spending_rate         = 1/20,
-      portfolio_value       = 20,
-      portfolio_return_mean = 0.07,
-      portfolio_return_sd   = 0.20,
-      expected_lifetime     = 1 / (log(2) / 28.1)
-    )
-
-  expect_equal(round(ruin_probability, 3), 0.268)
-
-
-  ruin_probs <- 
-    calc_retirement_ruin(
-      spending_rate         = seq(0.02, 0.07, by = 0.01),
-      portfolio_value       = 20,
-      portfolio_return_mean = 0.07,
-      portfolio_return_sd   = 0.20,
-      expected_lifetime     = 1 / (log(2) / 28.1)
-    )
-
-    ruin_probs
-    
-})
-   
-
-
-
+  expect_error(incomplete_gamma(a = 0, c = 0))
   
+  expect_equal(
+    calc_incomplete_gamma(a = 2, c = 3),
+    0.19914,
+    tolerance = 0.0001
+  )
+  
+  expect_equal(
+    calc_incomplete_gamma(a = 3, c = 2),
+    1.3533,
+    tolerance = 0.0001
+  )
+  
+  expect_equal(
+    calc_incomplete_gamma(a = -0.5, c = 1),
+    0.1781477,
+    tolerance = 0.0001
+  )
+  
+  expect_equal(calc_incomplete_gamma(a = 1, c = 0), 1)
+  expect_equal(calc_incomplete_gamma(a = 2, c = 0), 1)
+  expect_equal(calc_incomplete_gamma(a = 3, c = 0), 2)
 
+  expect_equal(
+    calc_a(0.01, 65, 88, 10),
+    17.88846,
+    tolerance = 0.0001
+  )
+
+  expect_equal(
+    calc_a(0.03, 65, 88, 10),
+    14.408,
+    tolerance = 0.0001
+  )
+
+  calc_retirement_ruin(
+    age                   = 65,
+    gompertz_mode         = 88,
+    gompertz_dispersion   = 10,
+    spending_rate         = 0.05,
+    portfolio_return_mean = 0.02,
+    portfolio_return_sd   = 0.20
+  ) |> 
+    expect_equal(0.3362243, tolerance = 0.00001)
+  
+  calc_retirement_ruin(
+    age                   = 65,
+    gompertz_mode         = 88,
+    gompertz_dispersion   = 10,
+    spending_rate         = 0.04,
+    portfolio_return_mean = 0.02,
+    portfolio_return_sd   = 0.20
+  ) |> 
+    expect_equal(0.2216641, tolerance = 0.00001)
+
+  calc_retirement_ruin(
+    age                   = 65,
+    gompertz_mode         = 88,
+    gompertz_dispersion   = 10,
+    spending_rate         = 0.04,
+    portfolio_return_mean = 0.02,
+    portfolio_return_sd   = 0.15
+  ) |> 
+    expect_equal(0.1744054, tolerance = 0.00001)
+})
+  
