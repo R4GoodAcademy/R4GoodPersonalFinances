330e3693dec173117de8842ee3e1e469495ff562
Kamil Wais <kamil.wais@gmail.com>
2025-03-30 09:21:48 +0200

    vectorized calc present value

--- R/calc_present_value.R
+++ R/calc_present_value.R
@@ -3,18 +3,27 @@ calc_present_value <- function(
   discount_rate
 ) {
 
-  data <- 
-    tibble::tibble(
-      cashflow = cashflow,
-      v        = seq_len(length(cashflow)) - 1
-    ) |> 
-    dplyr::mutate(
-      time_value_discount = 
-        1 / (
-          (1 + discount_rate)^(v - 0)
-        ),
-      pv = time_value_discount * cashflow
-    ) 
+  n_years        <- length(cashflow)
+  present_values <- vector("numeric", n_years)
+
+  for (i in 1:n_years) {
+    
+    future_income <- cashflow[i:n_years]
+
+    data <-
+      tibble::tibble(
+        cashflow = future_income,
+        v      = seq_len(length(future_income)) - 1
+      ) |>
+      dplyr::mutate(
+        time_value_discount =
+          1 / (
+            (1 + discount_rate)^(v - 0)
+          ),
+        pv = time_value_discount * cashflow
+      )
+    present_values[i] <- sum(data$pv)
+  }
   
-  sum(data$pv)
+  present_values
 }
