a6c85b8a8ec15fe7274dacec0c40692e6767aec2
Kamil Wais <kamil.wais@gmail.com>
2025-07-03 10:28:50 +0200

    added median expected allocation

--- tests/testthat/test-plot_expected_allocation.R
+++ tests/testthat/test-plot_expected_allocation.R
@@ -42,6 +42,7 @@ test_that("plotting expected allocation", {
     simulate_scenario(
       household    = household,
       portfolio    = portfolio,
+      debug = TRUE,
       current_date = test_current_date
     )
   
@@ -61,4 +62,60 @@ test_that("plotting expected allocation", {
     accounts = "taxadvantaged"
   ); if (interactive()) print(plot)
   vdiffr::expect_doppelganger("plot3", plot)
+})
+
+test_that("plotting expected allocation for Monte Carlo samples", {
+
+  skip_on_cran()
+  
+  older_member <- HouseholdMember$new(
+    name       = "older",  
+    birth_date = "1980-02-15"
+  )  
+  older_member$mode       <- 80
+  older_member$dispersion <- 10
+
+  younger_member <- HouseholdMember$new(
+    name       = "younger",  
+    birth_date = "1990-07-15"
+  )
+  younger_member$mode       <- 85
+  younger_member$dispersion <- 9
+
+  household <- Household$new()
+  household$add_member(older_member)  
+  household$add_member(younger_member)
+  
+  household$expected_income <- list(
+    "income_older" = c(
+      "members$older$age < 60 ~ 40000",
+      "members$older$age >= 60 ~ 20000"
+    ),
+    "income_younger" = c(
+      "members$younger$age < 50 ~ 40000"
+    )
+  )
+  household$expected_spending <- list(
+    "spending1" = c(
+      "TRUE ~ 10000"
+    )
+  )
+  test_current_date <- "2020-07-15"
+
+  portfolio <- generate_test_asset_returns(2)$returns
+
+  scenario <- 
+    simulate_scenario(
+      household    = household,
+      portfolio    = portfolio,
+      monte_carlo_samples = 3,
+      seeds = 1234,
+      debug = TRUE,
+      current_date = test_current_date
+    )
+  
+  plot <- plot_expected_allocation(
+    scenario = scenario
+  ); if (interactive()) print(plot)
+  vdiffr::expect_doppelganger("ea_mc", plot)
 })
